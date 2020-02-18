e416dc8823b884a09f50eec704b493aa83014da2
Johan Bueno <johan.s.bueno@gmail.com>
2020-02-18 18:31:32 -0500

    workoutSchema

---
+++ models/workout.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+
+    day:{
+        type: Date,
+        require:true
+     },
+    totalDuration : {
+         type: Number,
+         require:true
+     },
+     exercises : {
+         type: Number,
+         require:true
+     }
+
+});
+
+const WorkoutSchema = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = WorkoutSchema;
